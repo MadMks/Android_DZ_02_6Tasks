26c387aaa5016d3f98724c633a3337833bd55fab
Max <mks59k@gmail.com>
2019-09-10 22:04:47 +0300

    Create project

---
+++ task1/src/main/java/com/example/android_dz_02_6tasks/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_dz_02_6tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
