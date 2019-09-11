e70245ea6637bdf469efff6b836b07fce84ab66f
Max <mks59k@gmail.com>
2019-09-11 21:30:05 +0300

    Добавлен внутренний отступ для главного блока. Созданы 2 фрейма. Назначены цвета фреймам.

--- task2/src/main/java/com/example/task2/MainActivity.java
+++ task2/src/main/java/com/example/task2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
+
         // Главный контейнер.
         LinearLayout mainLL = new LinearLayout(this);
+        mainLL.setOrientation(LinearLayout.VERTICAL);
+        mainLL.setPadding(
+                (int)this.getResources().getDimension(R.dimen.mainLL_padding),
+                (int)this.getResources().getDimension(R.dimen.mainLL_padding),
+                (int)this.getResources().getDimension(R.dimen.mainLL_padding),
+                (int)this.getResources().getDimension(R.dimen.mainLL_padding)
+        );
+        
         //LayoutParams
         LinearLayout.LayoutParams mainLLParams =
                 new LinearLayout.LayoutParams(
@@ -24,10 +34,37 @@ public class MainActivity extends AppCompatActivity {
                 );
         mainLL.setLayoutParams(mainLLParams);
 
-        mainLL.setOrientation(LinearLayout.VERTICAL);
-        // test
-        mainLL.setBackgroundColor(Color.GREEN);
-        // TODO margin
+
+
+        // Верхний контейнер FrameLayout
+        FrameLayout frameLayoutOne = new FrameLayout(this);
+        frameLayoutOne.setBackgroundColor(
+                this.getResources().getColor(R.color.colorLightGreen)
+        );
+        // LayoutParams
+        LinearLayout.LayoutParams frameOneLLParams =
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                );
+        frameOneLLParams.weight = 1;
+        mainLL.addView(frameLayoutOne, frameOneLLParams);
+
+
+        // Нижний контейнер FrameLayout
+        FrameLayout frameLayoutTwo = new FrameLayout(this);
+        frameLayoutTwo.setBackgroundColor(
+                this.getResources().getColor(R.color.colorLightRed)
+        );
+        // LayoutParams
+        LinearLayout.LayoutParams frameTwoLLParams =
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                );
+        frameTwoLLParams.weight = 1;
+        mainLL.addView(frameLayoutTwo, frameTwoLLParams);
+
 
         // Назначем Активности главный контейнер.
         this.setContentView(mainLL);
