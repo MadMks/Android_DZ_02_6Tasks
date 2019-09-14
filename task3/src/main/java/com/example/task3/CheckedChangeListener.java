852e35cb4ce5d6b88266269166943f54292b0849
Max <mks59k@gmail.com>
2019-09-14 19:19:19 +0300

    Реализован выбор и установка выбранного цвета для фона. Реализована тестовая смена цвета текста

--- task3/src/main/java/com/example/task3/CheckedChangeListener.java
+++ task3/src/main/java/com/example/task3/CheckedChangeListener.java
@@ -1,23 +1,39 @@
 package com.example.task3;
 
+import android.graphics.Color;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+
+import androidx.annotation.ColorLong;
 
 public class CheckedChangeListener
         implements CompoundButton.OnCheckedChangeListener {
 
     private LinearLayout mainLLayout;
+    private Color layoutColor;
+    private Color textColor;
+
+    private int lColor;
 
     public CheckedChangeListener(LinearLayout mainLLayout) {
         this.mainLLayout = mainLLayout;
+        layoutColor = new Color();
     }
 
     @Override
     public void onCheckedChanged(CompoundButton button, boolean isChecked) {
 
+//        int red = mainLLayout.getBackground().
+
         switch (button.getId()){
 
             case R.id.cbRed:
+//                layoutColor
+//                        = Color
+//                        .valueOf(
+//                        255,
+//                                layoutColor,
+//                                layoutColor.blue())
                 break;
             case R.id.cbGreen:
                 break;
@@ -25,5 +41,7 @@ public class CheckedChangeListener
                 break;
         }
 
+        lColor = Color.rgb(255, 0, 0);
+        mainLLayout.setBackgroundColor(lColor);
     }
 }
--- task3/src/main/java/com/example/task3/MainActivity.java
+++ task3/src/main/java/com/example/task3/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.task3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout mainLLayout;
+    private CheckBox checkBoxRed;
+    private CheckBox checkBoxGreen;
+    private CheckBox checkBoxBlue;
 
-    private CheckedChangeListener checkedChangeListener;
+//    private CheckedChangeListener checkedChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
         mainLLayout = findViewById(R.id.mainLLayout);
 
-        checkedChangeListener = new CheckedChangeListener(mainLLayout);
+        checkBoxRed = findViewById(R.id.cbRed);
+        checkBoxGreen = findViewById(R.id.cbGreen);
+        checkBoxBlue = findViewById(R.id.cbBlue);
+
+//        checkedChangeListener = new CheckedChangeListener(mainLLayout);
+    }
+
+    public void ClickCheckBox(View view) {
+        int layoutColor;
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+        if (checkBoxRed.isChecked()) red = 255;
+        if (checkBoxGreen.isChecked()) green = 255;
+        if (checkBoxBlue.isChecked()) blue = 255;
+
+        layoutColor = Color.rgb(red, green, blue);
+
+        mainLLayout.setBackgroundColor(layoutColor);
+        checkBoxRed.setTextColor(layoutColor);
     }
 }
