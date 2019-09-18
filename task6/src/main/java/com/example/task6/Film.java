b732d076f3ff3afbfa55c780a5e1fa27d016c464
Max <mks59k@gmail.com>
2019-09-18 22:29:04 +0300

    Создание класса Film

---
+++ task6/src/main/java/com/example/task6/Film.java
@@ -0,0 +1,14 @@
+package com.example.task6;
+
+public class Film {
+
+    public String title;
+    public String genre;
+    public int year;
+
+    public Film(String title, String genre, int year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+}
